f153f64f1bcb4fb4b933cde3e829aa20fc272950
Steven Thacker <thacker.steve@gmail.com>
2016-12-07 21:22:30 -0600

    Removed unused RestTemplate reference in IndexController

--- src/main/java/com/warcrafttool/controller/IndexController.java
+++ src/main/java/com/warcrafttool/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class IndexController
@@ -25,7 +24,4 @@ public class IndexController
 
 	@Autowired
 	private static final XLogger LOGGER = XLoggerFactory.getXLogger(IndexController.class);
-
-	@Autowired
-	private RestTemplate restTemplate;
 }
